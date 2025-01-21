5a225c7c6e703204618694667c6a2e5135ee86b3
Zofia Nora <sophienora92@gmail.com>
2025-01-21 13:04:19 +0100

    created functions in js open and close Sidebar

---
+++ script.js
@@ -0,0 +1,9 @@
+const navbar = document.getElementById('navbar-side');
+
+function openSidebar() {
+    navbar.classList.add('show');
+}
+
+function closeSidebar() {
+    navbar.classList.remove('show');
+}
